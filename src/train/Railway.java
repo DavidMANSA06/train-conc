c6af9b9bc24677c9f52e7fa72b2c5b07a1e6cb41
m25gocal <mansa.gocaley@imt-atlantique.net>
2026-02-04 21:59:24 +0100

    version3

--- src/train/Railway.java
+++ src/train/Railway.java
@@ -8,40 +8,60 @@ package train;
  * @author Fabien Dagnat <fabien.dagnat@imt-atlantique.fr>
  * @author Philippe Tanguy <philippe.tanguy@imt-atlantique.fr>
  */
+
+
 public class Railway {
-	public final Element[] elements;
-	private Position pos;
-	
-	private int nbLR = 0;
-	private int nbRL = 0;
-	
+    public final Element[] elements;
+    
+    private int nbLR = 0;
+    private int nbRL = 0;
 
-	public Railway(Element[] elements) {
-		if(elements == null)
-			throw new NullPointerException();
-		
-		this.elements = elements;
-		for (Element e : elements)
-			e.setRailway(this);
-	}
-	
-	public synchronized void acquireLine(Direction dir) throws InterruptedException {
-	    if (dir == Direction.LR) {
-	        while (nbRL > 0) wait();
-	        nbLR++;
-	    } else {
-	        while (nbLR > 0) wait();
-	        nbRL++;
-	    }
-	}
+    public Railway(Element[] elements) {
+        if(elements == null)
+            throw new NullPointerException();
 
-	public synchronized void releaseLine(Direction dir) {
-	    if (dir == Direction.LR) nbLR--;
-	    else nbRL--;
-	    notifyAll();
-	}
-	
-	
+        this.elements = elements;
+        for (Element e : elements)
+            e.setRailway(this);
+    }
+
+    public synchronized void acquireLine(Direction dir) throws InterruptedException {
+        if (dir == Direction.LR) {
+            while (nbRL > 0) {
+                System.out.println("  [WAITING] Train LR waiting because " + nbRL + " train(s) RL on line");
+                wait();
+            }
+            nbLR++;
+            System.out.println("  [ACQUIRED] Line reserved for LR direction (nbLR=" + nbLR + ")");
+        } else {
+            while (nbLR > 0) {
+                System.out.println("  [WAITING] Train RL waiting because " + nbLR + " train(s) LR on line");
+                wait();
+            }
+            nbRL++;
+            System.out.println("  [ACQUIRED] Line reserved for RL direction (nbRL=" + nbRL + ")");
+        }
+    }
+
+    public synchronized void releaseLine(Direction dir) {
+        if (dir == Direction.LR) {
+            if (nbLR > 0) {
+                nbLR--;
+                System.out.println("  [RELEASED] Line released by LR train (nbLR=" + nbLR + ")");
+            } else {
+                System.err.println("Warning: nbLR already at 0!");
+            }
+        } else {
+            if (nbRL > 0) {
+                nbRL--;
+                System.out.println("  [RELEASED] Line released by RL train (nbRL=" + nbRL + ")");
+            } else {
+                System.err.println("Warning: nbRL already at 0!");
+            }
+        }
+        notifyAll();
+    }
+
     public Element getElement(int index) {
         if (index >= 0 && index < elements.length) {
             return elements[index];
@@ -61,20 +81,18 @@ public class Railway {
     public int size() {
         return elements.length;
     }
-	
 
-	@Override
-	public String toString() {
-		StringBuilder result = new StringBuilder();
-		boolean first = true;
-		for (Element e : this.elements) {
-			if (first)
-				first = false;
-			else
-				result.append("--");
-			result.append(e);
-		}
-		return result.toString();
-	}
-	
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for (Element e : this.elements) {
+            if (first)
+                first = false;
+            else
+                result.append("--");
+            result.append(e);
+        }
+        return result.toString();
+    }
 }
--- src/train/Section.java
+++ src/train/Section.java
@@ -7,26 +7,31 @@ package train;
  * @author Fabien Dagnat <fabien.dagnat@imt-atlantique.fr>
  * @author Philippe Tanguy <philippe.tanguy@imt-atlantique.fr>
  */
+
+
 public class Section extends Element {
-	public Section(String name) {
-		super(name);
-	}
+    public Section(String name) {
+        super(name);
+    }
 
-	@Override
-	protected boolean canEnter() {
-		
-		return currentOccupancy < 1; // Max 1 train
-	}
-	
-	@Override
-    public synchronized void enter(Train t) throws InterruptedException {
+    @Override
+    protected boolean canEnter() {
+        return currentOccupancy < 1; // Max 1 train
+    }
+
+    @Override
+    public void enter(Train t) throws InterruptedException {
         Direction d = t.getPosition().getDirection();
-        Element currentPos = t.getPosition().getPos();
-
-        if (currentPos instanceof Station) {
+        
+        // Trouver l'élément précédent (d'où vient le train)
+        Element previousElement = this.getNext(d == Direction.LR ? Direction.RL : Direction.LR);
+        
+        // Si on vient d'une Station, on réserve TOUTE la ligne pour cette direction
+        if (previousElement instanceof Station) {
             this.railway.acquireLine(d);
         }
         
+        // Entrer physiquement dans la section
         super.enter(t);
     }
 
@@ -35,9 +40,11 @@ public class Section extends Element {
         Direction d = t.getPosition().getDirection();
         Element nextElement = t.getPosition().getNextElement();
 
+        // Libération de la place physique
         super.leave(t);
 
-       
+        // On libère la ligne UNIQUEMENT si le prochain élément est une gare
+        // (= on a terminé de traverser toutes les sections)
         if (nextElement instanceof Station) {
             this.railway.releaseLine(d);
         }
